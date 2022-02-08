0841642d7b42688398f57ab22cdc2bb049a0da57
Drew Dahlman <info@drewdahlman.com>
2022-02-07 20:13:47 -0500

    adding prettier and husky

--- examples/wipe/scripts/foo.js
+++
@@ -1,9 +0,0 @@
-function init() {
-
-    let logo = document.querySelector("#logo");
-    logo.style.backgroundColor = "red";
-    BoltRouter.once('before-navigate', () => {
-        logo.style.backgroundColor = "white";
-    })
-}
-init();
---
+++ examples/wipe/scripts/homepage.js
@@ -0,0 +1,8 @@
+var logo = document.querySelector('#logo');
+logo.style.backgroundColor = 'red';
+BoltRouter.on('before-navigate', () => {
+  logo.style.backgroundColor = 'white';
+});
+BoltRouter.on('before-render', () => {
+  window.Page = null;
+});

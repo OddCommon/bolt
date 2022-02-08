ec487d99655796da419413a867628840fd65922a
Drew Dahlman <info@drewdahlman.com>
2022-02-08 07:30:35 -0500

    add gzip to build, improved script and link handling, added route props to navigate event, allow locking and unlocking by user

--- examples/wipe/scripts/index.js
+++ examples/wipe/scripts/index.js
@@ -4,48 +4,53 @@ let wrapper = document.querySelector('#wrapper');
 let transition = document.querySelector('#transition-wipe');
 let inTransit = false;
 
-const handleBeforeNaviate = () => {
-    console.log("Before Navigate")
-    transition.classList.add('in');
-    wrapper.classList.remove('active');
-    transition.addEventListener('transitionend', inTransitionComplete);
-}
+const handleBeforeNaviate = event => {
+  console.log(`Bolt: Before Navigate: to: ${event.to} from: ${event.from}`);
+  transition.classList.add('in');
+  wrapper.classList.remove('active');
+  transition.addEventListener('transitionend', inTransitionComplete);
+};
 
 const inTransitionComplete = () => {
-    console.log("In Transition Complete")
-    BoltRouter.resume();
-    transition.classList.add('out');
-    transition.removeEventListener('transitionend', inTransitionComplete);
-}
+  console.log('In Transition Complete');
+  BoltRouter.resume();
+  transition.classList.add('out');
+  transition.removeEventListener('transitionend', inTransitionComplete);
+};
 
 const handlePreRender = () => {
-    console.log("Pre Render");
-    BoltRouter.pause();
-}
+  console.log('Bolt: Pre Render');
+  BoltRouter.pause();
+};
 
 const handleRenderComplete = () => {
-    console.log("Render Complete")
-    window.scrollTo(0, 0);
-}
+  console.log('Bolt: Render Complete');
+  window.scrollTo(0, 0);
+};
 
 const handleLoaded = () => {
-    console.log("Load Complete")
-    transition.classList.add('out');
-    wrapper = document.querySelector('#wrapper');
-    transition.addEventListener('transitionend', cleanup);
-}
+  console.log('Bolt: Load Complete');
+  BoltRouter.lock();
+
+  transition.classList.add('out');
+  wrapper = document.querySelector('#wrapper');
+  transition.addEventListener('transitionend', cleanup);
+};
 
 const cleanup = () => {
-    transition.classList.remove('out');
-    transition.classList.remove('in');
-    wrapper.classList.add('active');
-    transition.removeEventListener('transitionend', cleanup);
-}
+  console.log('Out Transition Complete');
+  BoltRouter.unlock();
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    wrapper.classList.add('active');
-    BoltRouter.on('before-navigate', handleBeforeNaviate);
-    BoltRouter.on('before-render', handlePreRender);
-    BoltRouter.on('render-complete', handleRenderComplete);
-    BoltRouter.on('load-complete', handleLoaded);
+  transition.classList.remove('out');
+  transition.classList.remove('in');
+  wrapper.classList.add('active');
+  transition.removeEventListener('transitionend', cleanup);
+};
+
+document.addEventListener('DOMContentLoaded', event => {
+  wrapper.classList.add('active');
+  BoltRouter.on('before-navigate', handleBeforeNaviate);
+  BoltRouter.on('before-render', handlePreRender);
+  BoltRouter.on('render-complete', handleRenderComplete);
+  BoltRouter.on('load-complete', handleLoaded);
 });
--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,28 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import gzipPlugin from 'rollup-plugin-gzip';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'lib/index.js',
-	output: [
-		{
-			name: "BoltRouter",
-			file: 'build/bolt.js',
-			format: 'iife',
-			sourcemap: true
-		},
-		{
-			name: "BoltRouter",
-			file: 'build/bolt.module.js',
-			format: 'cjs', 
-			sourcemap: true,
-			exports: 'default'
-		}
-	],
-	plugins: [
-		resolve(),
-		commonjs(),
-		production && terser()
-	]
+  input: 'lib/index.js',
+  output: [
+    {
+      name: 'BoltRouter',
+      file: 'build/bolt.js',
+      format: 'iife',
+      sourcemap: true,
+    },
+    {
+      name: 'BoltRouter',
+      file: 'build/bolt.module.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'default',
+    },
+  ],
+  plugins: [resolve(), commonjs(), production && terser(), production && gzipPlugin()],
 };
